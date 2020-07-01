fa2692c425e1b64dfc42713944218603f1bdd718
Jonathan Célio <jonathan.clio@hotmail.com>
2020-07-01 00:51:50 -0300

    refactor: new features

--- packages/core/src/ui/providers/routing/package-detail/package-detail.graphql.types.ts
+++ packages/core/src/ui/providers/routing/package-detail/package-detail.graphql.types.ts
@@ -1,4 +1,4 @@
-import * as Types from '../../../shared-types';
+import * as Types from '../../../../../e2e/graphql/shared-types';
 
 import { PackageFragment } from '../../../common/fragment.graphql.types';
 export type Exact<T extends { [key: string]: any }> = { [K in keyof T]: T[K] };
